d418213d8a361c94a735ce24b8f18aa24e131eaf
Leland Richardson <leland@tech.pro>
2015-01-31 12:00:19 -0800

    fixed some tests for progress

--- test/progress.js
+++ test/progress.js
@@ -1,21 +1,18 @@
 var ReactFlux = require('../');
 var assert = require('chai').assert;
+var Promise = require('promise');
 
 var constants = ReactFlux.createConstants(['ONE', 'TWO'], 'STORE');
 
 var actions = ReactFlux.createActions({
   one: [constants.ONE, function () {
     return new Promise(function (resolve, reject) {
-      setTimeout(function () {
-        resolve();
-      },40);
+      setTimeout(resolve, 10);
     });
   }],
   two: [constants.TWO, function () {
     return new Promise(function (resolve, reject) {
-      setTimeout(function () {
-        resolve();
-      }, 0);
+      setTimeout(resolve, 0);
     });
   }]
 });
@@ -27,13 +24,24 @@ describe("progress", function () {
     assert.isFalse(ReactFlux.progress.of(constants.TWO));
   });
 
-  it("should be true when in progress", function () {
+  it("should be true when in progress", function ( done ) {
+    assert.isFalse(ReactFlux.progress.of(constants.ONE));
     actions.one();
     assert.isTrue(ReactFlux.progress.of(constants.ONE));
+    setTimeout(function () {
+      try {
+        assert.isTrue(ReactFlux.progress.of(constants.ONE));
+        done();
+      } catch ( e ) {
+        done(e);
+      }
+    }, 0);
+
   });
 
   it("should be false when progress is done", function ( done ) {
     actions.two();
+    assert.isTrue(ReactFlux.progress.of(constants.TWO));
     setTimeout(function () {
       try {
         assert.isFalse(ReactFlux.progress.of(constants.TWO));
@@ -41,7 +49,7 @@ describe("progress", function () {
       } catch ( e ) {
         done(e);
       }
-    }, 50);
+    }, 10);
   });
 
 });
