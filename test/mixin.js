448b3ec7435516b56d999e71856dbda02e55cf1e
kjda <khaled.jouda@ymail.com>
2015-02-14 17:43:26 +0100

    removing react

--- test/mixin.js
+++ test/mixin.js
@@ -1,13 +1,12 @@
 var assert = require('chai').assert;
 var sinon = require("sinon");
 var Flux = require('../');
-var React = require('react');
 
 describe("Mixin", function(){
-	
+
 	it("Flux should provide mixin interface", function(){
 		assert.isFunction(Flux.mixin);
-		
+
 		var mixin = Flux.mixin(Flux.createStore());
 		assert.isFunction(mixin.componentDidMount, 'mixin.componentDidMount is misiing');
 		assert.isFunction(mixin.componentWillUnmount, 'mixin.componentWillUnmount is missing');
