3292082a1d340b3b3d40a94bcad6480a8797a14b
jouda <jouda@mesmo.org>
2015-02-17 17:35:35 +0100

    indentation

--- test/configs.js
+++ test/configs.js
@@ -4,69 +4,69 @@ var assert = require('chai').assert;
 
 describe("configs", function(){
 
-  it("constant generation should be configurable", function(){
-    ReactFlux.configs.constants.setSeparator(':');
-    ReactFlux.configs.constants.setSuccessSuffix('OK');
-    ReactFlux.configs.constants.setFailSuffix('ERROR');
-    ReactFlux.configs.constants.setAfterSuffix('DONE');
+	it("constant generation should be configurable", function(){
+		ReactFlux.configs.constants.setSeparator(':');
+		ReactFlux.configs.constants.setSuccessSuffix('OK');
+		ReactFlux.configs.constants.setFailSuffix('ERROR');
+		ReactFlux.configs.constants.setAfterSuffix('DONE');
 
-    var fooConstants = ReactFlux.createConstants(['ONE'], 'FOO');
-    assert.equal(fooConstants.ONE_OK, 'FOO:ONE:OK');
-    assert.equal(fooConstants.ONE_ERROR, 'FOO:ONE:ERROR');
-    assert.equal(fooConstants.ONE_DONE, 'FOO:ONE:DONE');
+		var fooConstants = ReactFlux.createConstants(['ONE'], 'FOO');
+		assert.equal(fooConstants.ONE_OK, 'FOO:ONE:OK');
+		assert.equal(fooConstants.ONE_ERROR, 'FOO:ONE:ERROR');
+		assert.equal(fooConstants.ONE_DONE, 'FOO:ONE:DONE');
 
-    ReactFlux.configs.constants.resetToDefaults();
+		ReactFlux.configs.constants.resetToDefaults();
 
-    var barConstants = ReactFlux.createConstants(['TWO'], 'BAR');
-    assert.equal(barConstants.TWO_SUCCESS, 'BAR_TWO_SUCCESS');
-    assert.equal(barConstants.TWO_FAIL, 'BAR_TWO_FAIL');
-    assert.equal(barConstants.TWO_AFTER, 'BAR_TWO_AFTER');
+		var barConstants = ReactFlux.createConstants(['TWO'], 'BAR');
+		assert.equal(barConstants.TWO_SUCCESS, 'BAR_TWO_SUCCESS');
+		assert.equal(barConstants.TWO_FAIL, 'BAR_TWO_FAIL');
+		assert.equal(barConstants.TWO_AFTER, 'BAR_TWO_AFTER');
   });
 
 
 
   it("complains if Constants separator is not a string", function(){
-    assert.throw(function(){
-      ReactFlux.configs.constants.setSeparator({});
-    }, 'Constants.separator must be a non empty string');
+		assert.throw(function(){
+		  ReactFlux.configs.constants.setSeparator({});
+		}, 'Constants.separator must be a non empty string');
 
-    assert.throw(function(){
-      ReactFlux.configs.constants.setSeparator('');
-    }, 'Constants.separator must be a non empty string');
+		assert.throw(function(){
+		  ReactFlux.configs.constants.setSeparator('');
+		}, 'Constants.separator must be a non empty string');
 
   });
 
 
   it("complains if successSuffix is not a string", function(){
-    assert.throw(function(){
-      ReactFlux.configs.constants.setSuccessSuffix({});
-    }, 'Constants.successSuffix must be a non empty string');
+		assert.throw(function(){
+		  ReactFlux.configs.constants.setSuccessSuffix({});
+		}, 'Constants.successSuffix must be a non empty string');
 
-    assert.throw(function(){
-      ReactFlux.configs.constants.setSuccessSuffix('');
-    }, 'Constants.successSuffix must be a non empty string');
+		assert.throw(function(){
+		  ReactFlux.configs.constants.setSuccessSuffix('');
+		}, 'Constants.successSuffix must be a non empty string');
 
   });
 
   it("complains if failSuffix is not a string", function(){
-    assert.throw(function(){
-      ReactFlux.configs.constants.setFailSuffix({});
-    }, 'Constants.failSuffix must be a non empty string');
+		assert.throw(function(){
+		  ReactFlux.configs.constants.setFailSuffix({});
+		}, 'Constants.failSuffix must be a non empty string');
 
-    assert.throw(function(){
-      ReactFlux.configs.constants.setFailSuffix('');
-    }, 'Constants.failSuffix must be a non empty string');
+		assert.throw(function(){
+		  ReactFlux.configs.constants.setFailSuffix('');
+		}, 'Constants.failSuffix must be a non empty string');
 
   });
 
   it("complains if afterSuffix is not a string", function(){
-    assert.throw(function(){
-      ReactFlux.configs.constants.setAfterSuffix({});
-    }, 'Constants.afterSuffix must be a non empty string');
+		assert.throw(function(){
+		  ReactFlux.configs.constants.setAfterSuffix({});
+		}, 'Constants.afterSuffix must be a non empty string');
 
-    assert.throw(function(){
-      ReactFlux.configs.constants.setAfterSuffix('');
-    }, 'Constants.afterSuffix must be a non empty string');
+		assert.throw(function(){
+		  ReactFlux.configs.constants.setAfterSuffix('');
+		}, 'Constants.afterSuffix must be a non empty string');
 
   });
 
--- webpack.config.js
+++ webpack.config.js
@@ -4,44 +4,44 @@ var UglifyJsPlugin = require('webpack/lib/optimize/UglifyJsPlugin');
 
 module.exports = {
 
-  context: __dirname + '/lib/',
-  
-  watch: true,
-  
-  entry: './index.js',
-  
-  output: {
-    path:  './dist/',
-    filename: 'react-flux.js',
-    library: 'ReactFlux',
-    libraryTarget: 'umd'
-  },
-  
-  resolve: {
-    extensions: ['', '.js'],
-  },
-  
-  module: {
+	context: __dirname + '/lib/',
+	
+	watch: true,
+	
+	entry: './index.js',
+	
+	output: {
+		path:	'./dist/',
+		filename: 'react-flux.js',
+		library: 'ReactFlux',
+		libraryTarget: 'umd'
+	},
+	
+	resolve: {
+		extensions: ['', '.js'],
+	},
+	
+	module: {
 
-  },
+	},
 
-  externals: {
-    immutable: {
-      root: 'Immutable',
-      commonjs: 'immutable',
-      commonjs2: 'immutable',
-      amd: 'immutable'
-    },
-    promise: true,
-    react: {
-      root: 'React',
-      commonjs: 'react',
-      commonjs2: 'react',
-      amd: 'react'
-    }   
-  },
-  
-  plugins: [
-    new UglifyJsPlugin()
-  ]
+	externals: {
+		immutable: {
+			root: 'Immutable',
+			commonjs: 'immutable',
+			commonjs2: 'immutable',
+			amd: 'immutable'
+		},
+		promise: true,
+		react: {
+			root: 'React',
+			commonjs: 'react',
+			commonjs2: 'react',
+			amd: 'react'
+		}	 
+	},
+	
+	plugins: [
+		new UglifyJsPlugin()
+	]
 };
