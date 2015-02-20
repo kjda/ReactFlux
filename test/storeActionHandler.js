41abb2211baf9c4d0bdfbd4d951652e8197b1f9e
kjda <khaled.jouda@ymail.com>
2015-02-20 10:39:55 +0100

    fixes #13

--- test/storeActionHandler.js
+++ test/storeActionHandler.js
@@ -5,42 +5,63 @@ var StoreActionHandler = require('../lib/storeActionHandler');
 
 describe("storeActionHandler", function(){
 
-  it("should work on store only", function(){
+	it("should work on store only", function(){
 		assert.throw(function(){
-		  new StoreActionHandler({});
+			new StoreActionHandler({});
 		}, 'StoreActionHandler expects first parameter to be a store');
-  });
+	});
 
-  it("expects second parameter to be a constant", function(){
+	it("expects second parameter to be a constant", function(){
 		assert.throw(function(){
-		  var store = ReactFlux.createStore();
-		  new StoreActionHandler(store, '');
+			var store = ReactFlux.createStore();
+			new StoreActionHandler(store, '');
 		}, 'StoreActionHandler expects second parameter to be a constant(string)');
-  });
+	});
 
-  it("should create a default getInitialState", function(){
+	it("should create a default getInitialState", function(){
 		var store = ReactFlux.createStore();
 		var actionHandler = new StoreActionHandler(store, 'FOO', {});
 		assert.isFunction(actionHandler.getInitialState);
-  });
+	});
 
-  it("it should complain if getInitialState is not a function", function(){
+	it("should have a setState", function(){
+		var store = ReactFlux.createStore();
+		var actionHandler = new StoreActionHandler(store, 'FOO', {});
+		assert.isFunction(actionHandler.setState);
+	});
+
+	it("should have a getState", function(){
+		var store = ReactFlux.createStore();
+		var actionHandler = new StoreActionHandler(store, 'FOO', {});
+		assert.isFunction(actionHandler.getState);
+	});
+
+	it("should have a parent", function(){
+		var store = ReactFlux.createStore();
+		var actionHandler = new StoreActionHandler(store, 'FOO', {});
+		assert.isObject(actionHandler.parent)
+		assert.isTrue(actionHandler.parent.isStore());
+	});
+
+	//@todo: test that setting state work
+
+	it("it should complain if getInitialState is not a function", function(){
 		assert.throw(function(){
-		  var store = ReactFlux.createStore();
-		  new StoreActionHandler(store, 'FOO', {
+			var store = ReactFlux.createStore();
+			new StoreActionHandler(store, 'FOO', {
 				getInitialState: {}
-		  });
+			});
 		}, 'StoreActionHandler expects getInitialState to be a function');
-  });
+	});
 
-  it("it should complain if before,success,error or after is not a function", function(){
+	it("it should complain if before,success,error or after is not a function", function(){
 		assert.throw(function(){
-		  var store = ReactFlux.createStore();
-		  new StoreActionHandler(store, 'FOO', {
+			var store = ReactFlux.createStore();
+			new StoreActionHandler(store, 'FOO', {
 				before: {}
-		  });
+			});
 		}, 'StoreActionHandler expects "before" to be a function');
-  });
+	});
 
 
 });
